2a7ec52c0909defbd74dabb109c8598f76c5aa4d
adamm <adam.spark199@gmail.com>
2020-06-07 18:54:12 +0300

    Encrypt function (Buggy at round 3)

--- IDEA.py
+++ IDEA.py
@@ -1,44 +1,101 @@
 import random
-import numpy
+import numpy as np
+import binascii
 
 KEY_SIZE = 32
-BLOCK_SIZE = 4
+BLOCK_SIZE = 4  # Plaintext = 64bits
 SUB_KEYS = 6
 SHIFT_BITS = 6
 ROUNDS = 5  # Round up the number, NO HALVES
 
 
 class IDEA:
-    def __init__(self, key):
-        self.key = random.getrandbits(KEY_SIZE)
-        self.rol = lambda val, r_bits, max_bits: \
-            (val << r_bits % max_bits) & (2 ** max_bits - 1) | \
-            ((val & (2 ** max_bits - 1)) >> (max_bits - (r_bits % max_bits)))
+    def __init__(self):
+        self.key = 3698278233  # random.getrandbits(KEY_SIZE)
         self.sub_keys = self.key_scheduler()
+
+        self.mul = lambda x, y: np.mod(x * y, 2**4 + 1)
+        self.add = lambda x, y: np.mod(x + y, 2**4)
+        self.xor = lambda x, y: x ^ y
 
     def key_scheduler(self):  # Prepares all sub keys for the rounds
         key_scheduler = IDEA_Key_Scheduler(self.key)
         return key_scheduler.prepare_key_schedule()
 
-    def generate_next_key(self):
-        self.key = self.rol(self.key, 25, KEY_SIZE)  # Generate new 128 bit key by shifting 25 bits left
+    def encrypt(self, plain_text):
+        """
+        X1 * K1
+        X2 + K2
+        X3 + K3
+        X4 * K4
+        Step 1 ^ Step 3
+        Step 2 ^ Step 4
+        Step 5 * K5
+        Step 6 + Step 7
+        Step 8 * K6
+        Step 7 + Step 9
+        Step 1 ^ Step 9
+        Step 3 ^ Step 9
+        Step 2 ^ Step 10
+        Step 4 ^ Step 10
 
-    def get_sub_keys(self):
-        key_string = str(bin(self.key))[2:]
-        for index in range(0, len(key_string), 16):
-            self.sub_keys.append(key_string[index: index + 16])  # Divide key into 8 sub keys
+        The input to the next round is Step 11 || Step 13 || Step 12 || Step 14, which becomes X1 || X2 || X3 || X4.
+        This swap between 12 and 13 takes place after each complete round, except the last complete round (4th round),
+        where the input to the final half round is Step 11 || Step 12 || Step 13 || Step 14.
+        :param plain_text:
+        :return:
+        """
 
-        self.next_sub_keys = self.sub_keys[6:8]
-        self.generate_next_key()
-        temp = []
-        key_string = str(bin(self.key))[2:]
-        for index in range(0, len(key_string), 16):
-            temp.append(key_string[index: index + 16])  # Divide key into 8 sub keys
+        #THIS TEST SAMPLE OUTPUT MUST MATCH https://www.geeksforgeeks.org/simplified-international-data-encryption-algorithm-idea/ output
 
-        for elem in temp[2:8]:
-            self.next_sub_keys.append(elem)
+        X = get_pt_bin_block_list(plain_text)
+        X = ["00001001", "00001100", "00001010", "00001100"]
+        K = self.sub_keys
+        print("Plaintext binary:" + str(X))
 
-        return self.sub_keys[:6]  # Returns list of sub keys
+        [print("Key: " + str(x)) for x in K]
+
+        step = ['0'] * 14
+        for i in range(0, ROUNDS - 1):
+            print("Round " + str(i + 1) + " input " + str(X))
+            print("Round " + str(i + 1) + " input key" + str(K[i]))
+            step[0] = (self.mul(int(X[0], 2), int(K[i][0], 2)))
+            step[1] = (self.add(int(X[1], 2), int(K[i][1], 2)))
+            step[2] = (self.add(int(X[2], 2), int(K[i][2], 2)))
+            step[3] = (self.mul(int(X[3], 2), int(K[i][3], 2)))
+            step[4] = (self.xor(step[0], step[2]))
+            step[5] = (self.xor(step[1], step[3]))
+            step[6] = (self.mul(step[4], int(K[i][4], 2)))
+            step[7] = (self.add(step[5], step[6]))
+            step[8] = (self.mul(step[7], int(K[i][5], 2)))
+            step[9] = (self.add(step[6], step[8]))
+            step[10] = (self.xor(step[0], step[8]))
+            step[11] = (self.xor(step[2], step[8]))
+            step[12] = (self.xor(step[1], step[9]))
+            step[13] = (self.xor(step[3], step[9]))
+            #for y in range(14):
+            #    print("Step "+str(y+1)+": "+str(bin(int(step[y])))[2:])
+
+            X = [str(bin(int(step[10])))[2:], str(bin(int(step[12])))[2:], str(bin(int(step[11])))[2:],
+                 str(bin(int(step[13])))[2:]]  # Swap step 12 and 13
+
+            for j in range(0, len(X)):
+                X[j] = '0' * (4 - len(X[j])) + X[j]
+
+            print("Round " + str(i + 1) + " output " + str(X)+"\n---------------")
+
+        """X1 * K1
+           X2 + K2
+           X3 + K3
+           X4 * K4"""
+        X = [str(bin(int(step[10])))[2:], str(bin(int(step[11])))[2:], str(bin(int(step[12])))[2:],
+             str(bin(int(step[13])))[2:]]
+        result = []
+        result.append(self.mul(int(X[0], 2), int(K[ROUNDS - 1][0], 2)))
+        result.append(self.add(int(X[1], 2), int(K[ROUNDS - 1][1], 2)))
+        result.append(self.add(int(X[2], 2), int(K[ROUNDS - 1][2], 2)))
+        result.append(self.mul(int(X[3], 2), int(K[ROUNDS - 1][3], 2)))
+        print(result)
 
 
 """ PSEUDO for Key-Scheduler
@@ -59,9 +116,9 @@ Sub 5 1111 1101 0110 0111 0111 0001
 3. Shift main key = new key
 4. Take 6-len(new key) from shifted key and add to the end of new-sub-key2
 5. repeat from 2
+
+By Fucking Adam
 """
-
-
 class IDEA_Key_Scheduler:
     def __init__(self, key_int):
         self.key_int = key_int
@@ -79,7 +136,7 @@ class IDEA_Key_Scheduler:
             temp = key_bin_list
             del temp[:to_remove]
             new_sub_key = temp
-            self.key_int = self.shift(self.key_int, SHIFT_BITS, BLOCK_SIZE * 8)  # Make new shifted key
+            self.key_int = self.shift(self.key_int, SHIFT_BITS, KEY_SIZE)  # Make new shifted key
             key_bin_list = get_key_bin_list(self.key_int)
             to_remove = SUB_KEYS - len(new_sub_key) if SUB_KEYS - len(new_sub_key) >= 0 else 0
             [new_sub_key.append(x) for x in key_bin_list[:to_remove]]
@@ -87,6 +144,28 @@ class IDEA_Key_Scheduler:
 
         self.sub_keys_list[-1] = self.sub_keys_list[-1][0:SUB_KEYS - 2]
         return self.sub_keys_list
+
+
+def get_pt_bin_block_list(plain_text):  # 4 Blocks 16 bit each
+    pt_block_list = []
+
+    temp = ' '.join(item[2:] for item in map(bin, plain_text.encode('ascii')))  # Binary plain text string
+
+    temp_list = []
+    for index in range(0, len(temp), 8):
+        temp_list.append(temp[index: index + 8])  # Divide plain text into bytes
+    for i in range(len(temp_list)):
+        temp_list[i] = temp_list[i].replace(" ", "")  # Remove white spaces from bytes
+        temp_list[i] = ''.join(['0' * (8 - len(temp_list[i])) if len(temp_list[i]) < 8 else '']) + temp_list[i]  # Add
+        # missing zeros for prefix to binary key string making each char 8 bits
+
+    for i in range(8 - len(temp_list)):  # Expand list to 8 bytes(64bits)
+        temp_list.insert(0, '0' * 8)
+    ########################
+    for i in range(0, len(temp_list), 2):  # Combine every 2 bytes to form 16 bits blocks
+        pt_block_list.append(''.join([s for s in temp_list[i:i + 2]]))
+
+    return pt_block_list
 
 
 def get_key_bin_list(key_int):
@@ -107,8 +186,8 @@ def get_key_bin_list(key_int):
     return key_bin_list
 
 
-cryptor = IDEA(3698278233)
-
+cryptor = IDEA()
+# get_pt_bin_block_list('adam1234')
 # key_int = 3698278233  # ["1101", "1100", "0110", "1111", "0011", "1111", "0101", "1001"]
 # sched = IDEA_Key_Scheduler(key_int)
-[print("Key: " + str(x)) for x in cryptor.sub_keys]
+cryptor.encrypt('adam')
